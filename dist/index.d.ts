1bfd95a54461031e284925b9e06e01f7fc0a3680
Sam Claus <samterainsights@gmail.com>
2017-02-06 23:33:17 -0500

    Generated changes.

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,6 +1,7 @@
 import { Authenticator } from './Authenticator';
 import { Signer } from './Signer';
 import { ECDHResult } from './SecretDerivation';
+export { ExternalKeyPair } from './Crypto';
 export declare function createAuthenticator(): Authenticator;
 export declare function createSigner(): Signer;
 export declare function deriveHMACSecret(serverPublic: string): PromiseLike<ECDHResult>;
